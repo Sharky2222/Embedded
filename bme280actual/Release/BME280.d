3a84bde419793c35d42b344cc00f5ba3a444bf0e
SanjayThallam <60016462+SanjayThallam@users.noreply.github.com>
2021-12-15 15:44:50 -0500

    screen code added in

---
+++ bme280actual/Release/BME280.d
@@ -0,0 +1,45 @@
+# FIXED
+
+BME280.obj: ../BME280.c
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stddef.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h
+BME280.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h
+BME280.obj: ../BME280.h
+BME280.obj: ../i2c.h
+
+../BME280.c:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stddef.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h:
+
+../BME280.h:
+
+../i2c.h:
+
--- bme280actual/main.c
+++ bme280actual/main.c
@@ -3,7 +3,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdlib.h>
 
+#include "SSD1306.h"
 #include "TI_USCI_I2C_master.h"
 #include "i2c.h"
 #include "BME280.h"
@@ -19,6 +21,15 @@ volatile long control = 0;               //Controls which ADC configurations to 
 
 char result[100];                        //Used for displaying UART to console
 
+char lightChar[10];
+char moistChar[10];
+char tempChar[10];
+char humidChar[10];
+char pressChar[10];
+
+#define ANUMBER     19052019
+#define MAX_COUNT   4.2e9
+
 void ConfigureAdc_water(void);
 void ConfigureAdc_light(void);
 
@@ -27,6 +38,7 @@ void ConfigClocks(void);
 void port_init();
 void strreverse(char* begin, char* end);
 void itoa(int value, char* str, int base);
+void display_startop(void);
 /**
  * main.c
  */
@@ -35,20 +47,22 @@ static volatile int TimeDelay; //this will state the delay value
 
 #define greenled BIT0
 int main(void)
-
 {
-
     int pressure = 0;
     int temperature = 0;
     int humidity = 0;
 
-    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
     port_init();
     ConfigClocks();
     uart_init();
     i2c_init(0x77);
     BME280_init();
+    i2c_init(0x3C);
+    ssd1306_init();
+    ssd1306_clearDisplay();
+    display_startup();
 
     _delay_cycles(5);                    // Wait for ADC Ref to settle
 
@@ -59,16 +73,16 @@ int main(void)
             ConfigureAdc_light();
             ADC10CTL0 |= ENC + ADC10SC +MSC;                   // Converter Enable, Sampling/conversion start
             while((ADC10CTL0 & ADC10IFG) == 0);                // check the Flag, while its low just wait
-            _delay_cycles(2000000);                            // delay for about 1 second to allow time to read data
+            _delay_cycles(15000000);                            // delay for about 1 second to allow time to read data
             light_Raw = ADC10MEM;                              // read the converted data into a variable
             ADC10CTL0 &= ~ADC10IFG;                            // clear the flag
             //itoa((light_Raw),result,10);                       // format the light_Raw data into a string to be read on console
 
 
-            if(light_Raw <= 250)  lightLevel = 3;                           //Dark out
-            else if(light_Raw >= 250 && light_Raw < 500) lightLevel = 2;    //Little Sun
-            else if(light_Raw >= 500 && light_Raw < 700 ) lightLevel = 1;   //Partly Sunny
-            else if(light_Raw >= 800) lightLevel = 0;                       //Sunny Day
+            if(light_Raw <= 250)  lightLevel = 0;                           //Dark out
+            else if(light_Raw >= 250 && light_Raw < 500) lightLevel = 1;    //Little Sun
+            else if(light_Raw >= 500 && light_Raw < 700 ) lightLevel = 2;   //Partly Sunny
+            else if(light_Raw >= 800) lightLevel = 3;                       //Sunny Day
 
             itoa((lightLevel),result,10);
             int acount =0;
@@ -82,6 +96,7 @@ int main(void)
             //lightLevel = lightLevel;   //for graphing
             UCA0TXBUF = 'l';
             P2OUT ^= BIT1;
+            sprintf(lightChar, "%d", lightLevel);
             control = 1;    //change the control variable to go to the next sensor
 
         }
@@ -90,7 +105,7 @@ int main(void)
             ConfigureAdc_water();
             ADC10CTL0 |= ENC + ADC10SC +MSC;       // Converter Enable, Sampling/conversion start
             while((ADC10CTL0 & ADC10IFG) == 0);    // check the Flag, while its low just wait
-            _delay_cycles(2000000);                // delay for about 1 second to allow time to read data
+            _delay_cycles(15000000);                // delay for about 1 second to allow time to read data
             water_Raw = ADC10MEM;                  // read the converted data into a variable
             ADC10CTL0 &= ~ADC10IFG;                // clear the flag
             //itoa((water_Raw),result,10);           // format the water_Raw data into a string to be read on console
@@ -112,16 +127,17 @@ int main(void)
             }
             //waterLevel = waterLevel;     //for graphing
             UCA0TXBUF = 'm';
+            sprintf(moistChar, "%d", waterLevel);
             control = 2;    //change the control variable to go to the previous sensor
         }
 
         else if(control == 2)//temp
         {
-
+            i2c_init(0x77);
             while((ADC10CTL0 & ADC10IFG) == 0);
             ADC10CTL0 &= ~ADC10IFG;
             BME280_Data bmeData = BME280_read();
-            _delay_cycles(2000000);
+            _delay_cycles(15000000);
             temperature = bmeData.cTemperatureFInt;
             int hundreds = temperature/100;
             int tens = temperature/10;
@@ -158,7 +174,7 @@ int main(void)
 
             }
             UCA0TXBUF='t';
-
+            sprintf(tempChar, "%d", temperature);
 
 
             control = 3;
@@ -167,10 +183,10 @@ int main(void)
 
         else if(control == 3)//humidiity
         {
-
+            i2c_init(0x77);
             while((ADC10CTL0 & ADC10IFG) == 0);
             BME280_Data bmeData = BME280_read();
-            _delay_cycles(2000000);
+            _delay_cycles(15000000);
             humidity = bmeData.cHumidityInt;
 
             int hundreds = humidity/100;
@@ -207,17 +223,17 @@ int main(void)
             }
             UCA0TXBUF='h';
 
-
+            sprintf(humidChar, "%d", humidity);
 
             control = 4;
         }
 
         else if(control == 4)//pressure
         {
-
+            i2c_init(0x77);
             while((ADC10CTL0 & ADC10IFG) == 0);
             BME280_Data bmeData = BME280_read();
-            _delay_cycles(2000000);
+            _delay_cycles(15000000);
             pressure = bmeData.cPressureInt/100;
             //printf("%d", pressure);
 
@@ -235,31 +251,41 @@ int main(void)
                 UCA0TXBUF = result[acount++] ;                 //Transmit the received data.
 
             }
-            /*
             itoa((tens),result,10);
-
             acount =0;
             while(result[acount]!='\0')
             {
                 while((IFG2 & UCA0TXIFG)==0);                  //Wait Unitl the UART transmitter is ready
                 UCA0TXBUF = result[acount++] ;                 //Transmit the received data.
-
             }
             itoa((ones),result,10);
-
             acount =0;
             while(result[acount]!='\0')
             {
                 while((IFG2 & UCA0TXIFG)==0);                  //Wait Unitl the UART transmitter is ready
                 UCA0TXBUF = result[acount++] ;                 //Transmit the received data.
-
-            }*/
+            }
             UCA0TXBUF='p';
-
-
-
+            sprintf(pressChar, "%d", pressure);
             control = 0;
         }
+
+
+        //oled controls
+        i2c_init(0x3C);
+        ssd1306_clearDisplay();
+        ssd1306_printText(0, 1, "Garden Status");
+        ssd1306_printText(0, 2, "Light Level:");
+        ssd1306_printText(0, 3, "Soil Moisture:");
+        ssd1306_printText(0, 4, "Temperature (F):");
+        ssd1306_printText(0, 5, "Humidity (%):");
+        ssd1306_printText(0, 6, "Pressure (kPa):");
+
+        ssd1306_printText(100,2,lightChar);
+        ssd1306_printText(100,3,moistChar);
+        ssd1306_printText(100,4,tempChar);
+        ssd1306_printText(100,5,humidChar);
+        ssd1306_printText(100,6,pressChar);
 
     }
 
@@ -341,8 +367,17 @@ void itoa(int value, char* str, int base) {  //Function to convert the signed in
 }
 
 void port_init(){
+    P1OUT = 0x00;
     P1SEL |= BIT1 + BIT2;            // select non-GPIO  usage for Pins 1 and 2
     P1SEL2 |= BIT1 + BIT2;           // Select UART usage of Pins 1 and 2
 }
 
-
+void display_startup()
+{
+    ssd1306_printText(0, 1, "Garden Status");
+    ssd1306_printText(0, 2, "Light Level:");
+    ssd1306_printText(0, 3, "Soil Moisture:");
+    ssd1306_printText(0, 4, "Temperature (F):");
+    ssd1306_printText(0, 5, "Humidity (%):");
+    ssd1306_printText(0, 6, "Pressure (kPa):");
+}
